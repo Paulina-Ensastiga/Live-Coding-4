76b5fccf8e13676d5c47a9e32fbf38d3b156442b
Paulina-Ensastiga <celypau@gmail.com>
2023-07-07 13:12:33 -0600

    Ejercicio 4

---
+++ Codigo4.java
@@ -0,0 +1,50 @@
+package org.generation.cd4;
+
+public class Codigo4 {
+	//Celia Paulina Ensastiga Pichardo Calificación: 5
+
+		/*FizzBuzz
+		En este reto reforzaras tus conocimientos en Java, manejando ciclos, condicionales e impresión por consola.
+
+		1.- Construye un programa que imprima números del 1 al 100.
+
+		2.- Cuando el número sea múltiplo de 3 imprimir Fizz en lugar del número.
+
+		3.- Cuando el número sea un múltiplo de 5 imprimir Buzz en lugar del número.
+
+		4.- Cuando el número sea múltiplo de 3 y de 5 imprimir FizzBuzz en lugar del número.
+
+		5.- El programa no recibira inputs y cada número debe ir impreso en una línea.
+
+		6.- Debes escribir el código en una clase llamada FizzBuzz que contenga por lo menos un método, imprimir.
+
+		7.- En una clase Main, debes crear un objeto de dicha clase para imprimir el resultado.
+
+		Reglas
+		Debes compartir tu pantalla y mantener encendida tu cámara web
+		Durante el desafío debes explicar todos los pasos que realices.
+		Debes responder a las preguntas que haga el entrevistador.
+		
+		*/
+		public static void main (String []args) { 
+			for(int num = 1; num<101; num++) {
+				System.out.println(num);
+			if(num%3==0) {
+				System.out.println("Fizz");
+			}if(num%5==0) {
+				System.out.println("Buzz");
+			}if (num% 3==00 && num%5==0) {
+				System.out.println("FizzBuzz");
+			}
+		}
+	}
+	}
+
+
+	/*Paulina Trabaja de manera metódica
+	 *y se puede notar su lógica de programación.
+	  *Logra que el código funcione correctamente. Lo que puede mejorar es la práctica para acostumbrarse 
+	  *a la sintaxis del lenguaje.
+	 */
+
+
